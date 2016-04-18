d78ba4e563c3c477f2f6684370486dc0603dd5f7
kongxiangxiang <kongxiangxiang@kongxiangxiangs-MacBook-Pro.local>
2016-04-18 16:23:20 +0800

    2016-04-18

--- scripts/components/charts/Metrics.js
+++ scripts/components/charts/Metrics.js
@@ -4,12 +4,31 @@ import {Panel} from 'react-bootstrap'
 class Metrics extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {data:0};	
 	}
+
+	componentDidMount(){
+		const {refresh} = this.props || 0;
+		let _ref = this;
+		if(refresh){
+			setInterval(function(){
+				_ref.setState({data:_ref.state.data + 1});
+			},refresh);			
+		}
+	}
+
+	/*shouldComponentUpdate(nextProps, nextState) {
+		if(nextState.needRefresh && !this.state.needRefresh){
+			this.data = this.data + 1;
+			return true;
+		}
+		return false;
+	}*/
 
 	render() {
 		return (
-			<Panel header={this.props.title} bsStyle="primary">
-				<h2>10000</h2>
+			<Panel header={this.props.title} bsStyle="default">
+				<h2>{this.state.data}</h2>
 			</Panel>
 		);
 	}
--- scripts/containers/RealTimeContainer.js
+++ scripts/containers/RealTimeContainer.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Row,Col} from 'react-bootstrap';
+import {Row,Col,PageHeader,Label,small,Panel,Thumbnail} from 'react-bootstrap';
 import Metrics from '../components/charts/Metrics'
 
 class RealTimeContainer extends Component {
@@ -12,11 +12,66 @@ class RealTimeContainer extends Component {
     render() {
     	return (
     		<div>
-    			<Row>
-    				<Col lg={2} md={2} xs={2} lgOffset={1} mdOffset={1} xsOffset={1}>
-    					<Metrics title={"待出库订单总数"} />
-    				</Col>
-    			</Row>
+                <Row>
+                <Col lg={6} md={6} xs={6}>
+                    <Thumbnail>
+                        <h4>待出库总计</h4>
+                        <Row>
+                            <Col lg={4} md={4} xs={4}>
+                                <Metrics title={"订单总数"} refresh={500}/>
+                            </Col>
+                            <Col lg={4} md={4} xs={4}>
+                                <Metrics title={"已物流申报订单总数"} />
+                            </Col>
+                            <Col lg={4} md={4} xs={4}>
+                                <Metrics title={"物流申报放行订单总数"} />
+                            </Col>
+                            <Col lg={4} md={4} xs={4}>
+                                <Metrics title={"已商检申报订单总数"} />
+                            </Col>
+                            <Col lg={4} md={4} xs={4}>
+                                <Metrics title={"商检核销订单总数"} />
+                            </Col>             
+                        </Row>
+
+                    </Thumbnail>
+                </Col>
+                <Col lg={6} md={6} xs={6}>
+                <Thumbnail>
+                    <h4>异常总计</h4>
+                    <Row>
+                        <Col lg={3} md={3} xs={3}>
+                            <Metrics title={"接收订单异常"} refresh={500}/>
+                        </Col>
+                        <Col lg={3} md={3} xs={3}>
+                            <Metrics title={"物流申报异常"} refresh={500}/>
+                        </Col>
+                        <Col lg={3} md={3} xs={3}>
+                            <Metrics title={"物流回执异常"} refresh={500}/>
+                        </Col>
+                        <Col lg={3} md={3} xs={3}>
+                            <Metrics title={"商检出库异常"} />
+                        </Col>
+                        <Col lg={3} md={3} xs={3}>
+                            <Metrics title={"商检申报异常"} />
+                        </Col>
+                        <Col lg={3} md={3} xs={3}>
+                            <Metrics title={"商检回执异常"} />
+                        </Col>
+                        <Col lg={3} md={3} xs={3}>
+                            <Metrics title={"出库通知异常"} refresh={500}/>
+                        </Col>
+                    </Row>
+                </Thumbnail>
+                </Col>
+                </Row>
+                <h3>实时处理<small>(5分钟)</small></h3>
+                <Row>
+                    <Col lg={6} md={6} xs={6}>
+                    </Col>
+                    <Col lg={6} md={6} xs={6}>
+                    </Col>                    
+                </Row>
     		</div>
     	)
     }
